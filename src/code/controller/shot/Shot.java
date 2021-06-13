09e48a3f3f55e44e2cbbb4246d25371328eca4c5
gcroc <g.rochlitz@tum.de>
2021-06-13 17:36:03 +0200

    -

--- src/code/controller/shot/Shot.java
+++ src/code/controller/shot/Shot.java
@@ -1,15 +1,84 @@
 package code.controller.shot;
 
+import code.Dimension2D;
+import code.Point2D;
 import code.model.Spaceship;
 
 public class Shot {
-	
-	private Spaceship spaceship;
-	
-	public boolean detectCollision() {
-		return false;
+
+	private static final int DEFAULT_SHOT_WIDTH = 50; // YET TO DEFINE
+	private static final int DEFAULT_SHOT_HEIGHT = 25; // YET TO DEFINE
+
+	private static final int UP = 1;
+	private static final int DOWN = 0;
+
+	private Spaceship spaceship; // DO WE NEED THIS?
+	private String iconLocation;
+	private Dimension2D size = new Dimension2D(DEFAULT_SHOT_WIDTH, DEFAULT_SHOT_HEIGHT);
+	private Point2D position;
+	private int speed = 10;
+	private int direction;
+
+	public Shot(int direction) {
+		this.direction = direction;
+	}
+
+	public void setPosition(Dimension2D gameBoardSize, Point2D position) {
+		this.position = position;
 	}
 	
-	public void travel() {}
+	public boolean detectCollision(Spaceship spaceship) {
+		Point2D p1 = this.getPosition();
+		Dimension2D d1 = this.getSize();
+
+		Point2D p2 = spaceship.getPosition();
+		Dimension2D d2 = spaceship.getSize();
+
+		boolean above = p1.getY() + d1.getHeight() < p2.getY();
+		boolean below = p1.getY() > p2.getY() + d2.getHeight();
+		boolean right = p1.getX() + d1.getWidth() < p2.getX();
+		boolean left = p1.getX() > p2.getX() + d2.getWidth();
+
+		return !above && !below && !right && !left;
+	}
+
+	public Dimension2D getSize() {
+		return size;
+	}
+
+	public Point2D getPosition() {
+		return position;
+	}
+
+	public void travel(Dimension2D gameBoardSize) {
+		if (direction == UP) {
+			speed = Math.abs(speed) * -1;
+		} else if (direction == DOWN) {
+			speed = Math.abs(speed);
+		}
+		double maxY = gameBoardSize.getHeight();
+		// calculate delta between old coordinates and new ones based on speed and
+		// direction
+		double deltaY = this.speed;
+		double newY = this.position.getY() + deltaY;
+
+		// calculate position in case the boarder of the game board has been reached
+		if (newY < 0 || newY + this.size.getHeight() > maxY) {
+			// delete shot
+		}
+		// set coordinates
+		this.position = new Point2D(this.position.getX(), newY);
+	}
+
+	public String getIconLocation() {
+		return this.iconLocation;
+	}
+
+	public void setIconLocation(String iconLocation) {
+		if (iconLocation == null) {
+			throw new NullPointerException("The image of a spaceship cannot be null.");
+		}
+		this.iconLocation = iconLocation;
+	}
 
 }
--- src/code/model/Spaceship.java
+++ src/code/model/Spaceship.java
@@ -6,32 +6,71 @@ import code.controller.shot.Shot;
 
 public abstract class Spaceship {
 
-	protected static final int DEFAULT_SPACESHIP_WIDTH = 50; // YET TO DEFINE
-	protected static final int DEFAULT_SPACESHIP_HEIGHT = 25; // YET TO DEFINE
+	private static final int DEFAULT_SPACESHIP_WIDTH = 50; // YET TO DEFINE
+	private static final int DEFAULT_SPACESHIP_HEIGHT = 25; // YET TO DEFINE
 
 	private int minSpeed;
 	private int maxSpeed;
 	private int speed;
 	private boolean crunched;
-
-	private Point2D position;
-
+	private int lives;
+	private boolean firstRow;
 	private String iconLocation;
+	private Point2D position;
 	private Dimension2D size = new Dimension2D(DEFAULT_SPACESHIP_WIDTH, DEFAULT_SPACESHIP_HEIGHT);
 
-	protected Spaceship(Dimension2D gameBoardSize, Point2D position) {
+	public Spaceship(Dimension2D gameBoardSize, Point2D position) {
 		setPosition(gameBoardSize, position);
 	}
 
-	protected void setPosition(Dimension2D gameBoardSize, Point2D position) {
+	public void setPosition(Dimension2D gameBoardSize, Point2D position) {
 		this.position = position;
 	}
 	
-	public void shoot() {
-		Shot shot = new Shot();
+	public void shoot(int direction) {
+		Shot shot = new Shot(direction);
 	}
 	
-	public abstract void move();
+	public void move(Dimension2D gameBoardSize) {
+		double maxY = gameBoardSize.getHeight();
+		// calculate delta between old coordinates and new ones based on speed and
+		// direction
+		double deltaY = this.speed;
+		double newY = this.position.getY() + deltaY;
+
+		// calculate position in case the boarder of the game board has been reached
+		if (newY < 0 || newY + this.size.getHeight() > maxY) {
+			// delete shot
+		}
+		// set coordinates
+		this.position = new Point2D(this.position.getX(), newY);
+	}
 	
+	public void setLives(int lives) {
+		this.lives = lives;
+	}
+
+	public int getLives() {
+		return lives;
+	}
+
+	public Point2D getPosition() {
+		return position;
+	}
+
+	public Dimension2D getSize() {
+		return size;
+	}
+
+	public String getIconLocation() {
+		return this.iconLocation;
+	}
+
+	public void setIconLocation(String iconLocation) {
+		if (iconLocation == null) {
+			throw new NullPointerException("The image of a spaceship cannot be null.");
+		}
+		this.iconLocation = iconLocation;
+	}
 
 }
